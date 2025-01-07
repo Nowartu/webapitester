becafb7f368ee3447f1829c22fb7f2ae42417e07
artur <aartur.now@gmail.com>
2025-01-07 20:19:29 +0100

    Init

--- vite.config.mjs
+++ vite.config.mjs
@@ -33,25 +33,25 @@ export default defineConfig({
         name: "webapitest",
         "icons": [
           {
-            "src": "/pwa2-192x192.png",
+            "src": "/pwa-192x192.png",
             "sizes": "192x192",
             "type": "image/png",
             "purpose": "any"
           },
           {
-            "src": "/pwa2-512x512.png",
+            "src": "/pwa-512x512.png",
             "sizes": "512x512",
             "type": "image/png",
             "purpose": "any"
           },
           {
-            "src": "/pwa2-maskable-192x192.png",
+            "src": "/pwa-maskable-192x192.png",
             "sizes": "192x192",
             "type": "image/png",
             "purpose": "maskable"
           },
           {
-            "src": "/pwa2-maskable-512x512.png",
+            "src": "/pwa-maskable-512x512.png",
             "sizes": "512x512",
             "type": "image/png",
             "purpose": "maskable"
