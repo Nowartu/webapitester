bbeec5072196669b5c41eba2b19cbba47d704c00
artur <aartur.now@gmail.com>
2025-01-07 20:13:46 +0100

    Init

--- src/components/BatteryApi.vue
+++ src/components/BatteryApi.vue
@@ -1,9 +1,26 @@
 <script setup>
+import {ref} from 'vue'
 
+const battery = ref({
+  charging: null,
+  chargingTime: null,
+  dischargingTime: null,
+  level: null
+})
+
+navigator.getBattery().then(b => {
+  battery.value.charging = b.charging;
+  battery.value.chargingTime = b.chargingTime;
+  battery.value.dischargingTime = b.dischargingTime;
+  battery.value.level = b.level;
+})
 </script>
 
 <template>
-BatteryAPI
+  Charging: {{ battery.charging }} <br>
+  ChargingTime: {{ battery.chargingTime }} <br>
+  DischargingTime: {{ battery.dischargingTime }} <br>
+  Level: {{ battery.level }}
 </template>
 
 <style scoped lang="sass">
--- src/components/GeolocationComponent.vue
+++ src/components/GeolocationComponent.vue
@@ -1,9 +1,24 @@
 <script setup>
+import {ref} from 'vue';
 
+const location = ref({
+  longitude: 0,
+  latitude: 0,
+  accuracy: 0,
+})
+function success(pos){
+  const crd = pos.coords;
+  location.value.longitude = crd.longitude;
+  location.value.latitude = crd.latitude;
+  location.value.accuracy = crd.accuracy;
+}
+navigator.geolocation.getCurrentPosition(success)
 </script>
 
 <template>
-Geolocation
+  Longitude: {{ location.longitude }}<br>
+  Latitude: {{ location.latitude }}<br>
+  Accuracy: {{ location.accuracy }}
 </template>
 
 <style scoped lang="sass">
